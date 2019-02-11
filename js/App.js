f4eb40e546e478011eaa9d275281c9256227c977
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-11 13:46:20 +0100

    Add js

---
+++ js/App.js
@@ -0,0 +1,38 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '3818',
+  'X-Auth-Token': '35161c475db83fd1449b84ac2b3f5f49'
+};
+
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+  });
+
+function setupColumns(columns) {
+  columns.forEach(function (column) {
+	var col = new Column(column.id, column.name);
+    board.addColumn(col);
+    setupCards(col, column.cards);
+  });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
+
+function generateTemplate(name, data, basicElement) {
+  	var template = document.getElementById(name).innerHTML;
+  	var element = document.createElement(basicElement || 'div');
+  
+  	Mustache.parse(template);
+  	element.innerHTML = Mustache.render(template, data);
+  
+  	return element;
+}
