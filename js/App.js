38cbc8fcfc7630d60b4485327685598056e5fd6f
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-11 14:33:51 +0100

    Add scss

--- js/App.js
+++ js/App.js
@@ -1,3 +1,13 @@
+function generateTemplate(name, data, basicElement) {
+  	var template = document.getElementById(name).innerHTML;
+  	var element = document.createElement(basicElement || 'div');
+  
+  	Mustache.parse(template);
+  	element.innerHTML = Mustache.render(template, data);
+  
+  	return element;
+}
+
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
   'X-Client-Id': '3818',
@@ -14,25 +24,15 @@ fetch(baseUrl + '/board', { headers: myHeaders })
 
 function setupColumns(columns) {
   columns.forEach(function (column) {
-	var col = new Column(column.id, column.name);
+  var col = new Column(column.id, column.name);
     board.addColumn(col);
     setupCards(col, column.cards);
   });
 }
 
 function setupCards(col, cards) {
-	cards.forEach(function (card) {
+  cards.forEach(function (card) {
     var cardObj = new Card(card.id, card.name);
-  	col.addCard(cardObj);
-	});
-}
-
-function generateTemplate(name, data, basicElement) {
-  	var template = document.getElementById(name).innerHTML;
-  	var element = document.createElement(basicElement || 'div');
-  
-  	Mustache.parse(template);
-  	element.innerHTML = Mustache.render(template, data);
-  
-  	return element;
+    col.addCard(cardObj);
+  });
 }
